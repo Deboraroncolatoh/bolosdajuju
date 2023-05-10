d3c14dcbf7c6818083d910f384b014fe7aa1f885
Deboraroncolatoh <132912283+Deboraroncolatoh@users.noreply.github.com>
2023-05-10 10:44:50 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,21 @@
-<?php
-echo "Olá, mundo e turmaaa";
+<!DOCTYPE html>
+<html>
+  <head>
+    
+    <title> melhores musicas da aurora   </title>
+    
+  </head>
 
-?>
+<body>
+  <h1> primeiro álbum   </h1>  
+  <p> winter bird, running with the wolves, runaway, home e under the water </p>
+  <p> estas musicas representam sentimentos humanos e experiências sociais.
+      Principalmente a musica mais aclamada do álbum que segundo os fãs é a chamada Runaway, mas eu discordo pois prefiro Winter Bird </p>
+  
+  <a href ="#" class="button">Segundo álbum</a>
+ 
+  
+</body>
+
+
+</html>
